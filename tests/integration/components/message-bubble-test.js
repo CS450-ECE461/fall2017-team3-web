94e312cc749408f7f6c1b8e3f4292efd217238aa
Amos Endersen <amenders@iupui.edu>
2017-11-02 15:36:38 -0400

    created messages route and bubble component

---
+++ tests/integration/components/message-bubble-test.js
@@ -0,0 +1,24 @@
+import { moduleForComponent, test } from 'ember-qunit';
+import hbs from 'htmlbars-inline-precompile';
+
+moduleForComponent('message-bubble', 'Integration | Component | message bubble', {
+  integration: true
+});
+
+test('it renders', function(assert) {
+  // Set any properties with this.set('myProperty', 'value');
+  // Handle any actions with this.on('myAction', function(val) { ... });
+
+  this.render(hbs`{{message-bubble}}`);
+
+  assert.equal(this.$().text().trim(), '');
+
+  // Template block usage:
+  this.render(hbs`
+    {{#message-bubble}}
+      template block text
+    {{/message-bubble}}
+  `);
+
+  assert.equal(this.$().text().trim(), 'template block text');
+});
