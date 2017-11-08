4f8777f72b6b9202b0e0f3f90bd235717218598f
evrohuff <erh95@att.net>
2017-11-08 18:33:59 -0500

    Gatekeeper route & form setup

--- config/environment.js
+++ config/environment.js
@@ -9,9 +9,10 @@ module.exports = function(environment) {
     locationType: 'auto',
     EmberENV: {
       FEATURES: {
-        // Here you can enable experimental features on an ember canary build
-        // e.g. 'with-controller': true
+        // this is needed for the account adapter to work
+        'ds-improved-ajax': true
       },
+
       EXTEND_PROTOTYPES: {
         // Prevent Ember Data from overriding Date.parse.
         Date: false
@@ -21,6 +22,17 @@ module.exports = function(environment) {
     APP: {
       // Here you can pass flags/options to your application instance
       // when it is created
+    },
+
+    gatekeeper: {
+      baseUrl: 'http://localhost:5000',
+      startRoute: 'profile',
+      signInRoute: 'sign-in',
+
+      tokenOptions: {
+        client_id: '59ee923e1fd71c2ae68ade62',
+        client_secret: '1234567890'
+      }
     }
   };
 
