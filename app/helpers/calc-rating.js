1f1f4c71aada5476dd89a274d1d56dfd98662164
Mounir Karmada <mounkarm@iupui.edu>
2017-11-15 02:27:32 -0500

    addressing rating issue

--- app/helpers/calc-rating.js
+++ app/helpers/calc-rating.js
@@ -2,12 +2,16 @@ import Ember from 'ember';
 
 export function calcRating(params) {
 
-  let arr = params[Object.keys(params)[0]];
+  let arr = params[0];
   let sum = 0;
 
-  for(let i = 0; i < arr.length; i++) {
-    sum += arr[i];
+  if(arr.length === 0) {
+    return 0;
+  } else {
+    for(let i = 0; i < arr.length; i++) {
+      sum += arr[i];
+    }
   }
-  return (sum/arr.length);
+  return sum/arr.length;
 }
 export default Ember.Helper.helper(calcRating);
