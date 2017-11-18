5c887cdea10143201d8c9121b8eadcb7483c3ece
Amos Endersen <amenders@iupui.edu>
2017-11-18 10:04:48 -0500

    added dropdown toggle capability

--- app/controllers/search.js
+++ app/controllers/search.js
@@ -1,12 +1,25 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  arrowValue: "keyboard-arrow-down",
+
   actions: {
     search(param, type) {
       if (param !== '') {
         this.set("results", this.get('store').query(type, {skills: param }));
       } else {
         this.set("results", this.get('store').query(type, {skills: 'undefined'}));
+      }
+    },
+
+    toggleShowToggle(){
+      if (this.get("arrowValue") == "keyboard-arrow-down") {
+        this.set("arrowValue", "keyboard-arrow-up")
+        this.set("showToggle", true);
+      }
+      else {
+        this.set("arrowValue", "keyboard-arrow-down")
+        this.set("showToggle", false);
       }
     }
   }
