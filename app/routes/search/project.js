5b23054975951f877006dc358cb650dbd0ad2ea7
Mounir Karmada <mounkarm@iupui.edu>
2017-11-26 21:51:12 -0500

    Project page displays contributors models

--- app/routes/search/project.js
+++ app/routes/search/project.js
@@ -3,5 +3,12 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model(params) {
     return this.get('store').findRecord('project', params.project_id);
+  },
+
+  setupController(controller, model) {
+    controller.set('project', model);
+    this.store.findAll('user').then(function(users) {
+      controller.set('users', users);
+    });
   }
 });
