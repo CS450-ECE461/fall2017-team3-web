eb3a1adfbe408797a97e104f4d739f20508b60ba
Mounir Karmada <mounkarm@iupui.edu>
2017-11-28 23:42:02 -0500

    switched to RESTAdapter

--- mirage/config.js
+++ mirage/config.js
@@ -2,55 +2,42 @@ export default function() {
   this.namespace = 'api';
 
   let users = [{
-        type: 'user',
         id: 'user_one@example.com',
-        attributes: {
           name: 'Phyllis Kingsley',
           rating: [3.5, 5],
           skills: ['Photography', 'Adobe Design Suite', 'Painting'],
           email: 'user_one@example.com',
           image: 'https://upload.wikimedia.org/wikipedia/commons/c/cb/Crane_estate_(5).jpg',
           projects: ['project_1','project_3']
-        }
       }, {
-        type: 'user',
         id: 'user_two@example.com',
-        attributes: {
           name: 'Daniel Yeager',
           rating: [5.0, 4.5, 3.5],
           skills: ['Landscaping', 'Plumbing', 'Hanging out while doing the above'],
           email: 'user_two@example.com',
           image: 'https://upload.wikimedia.org/wikipedia/commons/0/0e/Alfonso_13_Highrise_Tegucigalpa.jpg',
           projects: ['project_1','project_2']
-        }
       }, {
-        type: 'user',
         id: 'user_three@example.com',
-        attributes: {
           name: 'Adam Carpenter',
           rating: [1.2, 5],
           skills: ['roofing', 'construction', 'surveying'],
           email: 'user_three@example.com',
           image: 'https://upload.wikimedia.org/wikipedia/commons/f/f7/Wheeldon_Apartment_Building_-_Portland_Oregon.jpg',
           projects: ['project_1','project_2']
-        }
       }, {
-         type: 'user',
          id: 'user_four@example.com',
-         attributes: {
            name: 'James Cooper',
            rating: [3, 1.5],
            skills: ['sleeping', 'eating', 'Private Affairs'],
            email: 'user_four@example.com',
            image: 'https://upload.wikimedia.org/wikipedia/commons/f/f7/Wheeldon_Apartment_Building_-_Portland_Oregon.jpg',
            projects: ['project_3','project_2']
-    }
+
   }];
 
   let projects = [{
-    type: 'project',
     id: 'project_1',
-    attributes: {
       name: 'Messaging App',
       description: 'creating a messaging app to beat WhatsApp',
       skills: ['web design', 'web development', 'software testing'],
@@ -58,11 +45,8 @@ export default function() {
       status: 'active',
       collaborators: ['user_two@example.com','user_one@example.com'],
       image: 'http://lorempixel.com/400/200'
-    }
   }, {
-    type: 'project',
     id: 'project_2',
-    attributes: {
       name: 'Building a Shed',
       description: 'building an awesome shed out of plastic plastic plastic plastic plasticplastic plastic plastic plastic plastic plastic plastic plastic plastic plastic',
       skills: ['construction', 'Land Surveying'],
@@ -70,11 +54,8 @@ export default function() {
       status: 'active',
       collaborators: ['user_three@example.com','user_one@example.com'],
       image: 'http://lorempixel.com/400/200'
-    }
   }, {
-    type: 'project',
     id: 'project_3',
-    attributes: {
       name: 'catering event',
       description: 'provide awesome food to an awesome event',
       skills: ['cooking', 'baking'],
@@ -82,37 +63,36 @@ export default function() {
       status: 'inactive',
       collaborators: ['user_three@example.com'],
       image: 'http://lorempixel.com/400/200'
-    }
   }];
 
 
   this.get('/projects', function(db, request) {
     if(request.queryParams.skills !== undefined) {
       let filteredProjects = projects.filter(function(i) {
-        return i.attributes.skills.toString().toLowerCase().indexOf(request.queryParams.skills.toString().toLowerCase()) !== -1;
+        return i.skills.toString().toLowerCase().indexOf(request.queryParams.skills.toString().toLowerCase()) !== -1;
       });
-      return { data: filteredProjects };
+      return { projects : filteredProjects};
     } else {
-      return { data: projects};
+      return { projects : projects};
     }
   });
 
   this.get('/users', function(db, request) {
     if(request.queryParams.skills !== undefined) {
       let filteredUsers = users.filter(function(i) {
-        return i.attributes.skills.toString().toLowerCase().indexOf(request.queryParams.skills.toString().toLowerCase()) !== -1;
+        return i.skills.toString().toLowerCase().indexOf(request.queryParams.skills.toString().toLowerCase()) !== -1;
       });
-      return { data: filteredUsers };
+      return { users: filteredUsers};
     } else {
-      return { data: users };
+      return { users: users };
     }
   });
 
   this.get('/users/:id', function (db, request) {
-    return { data: users.find((user) => request.params.id === user.id) };
+    return { users: users.find((user) => request.params.id === user.id) };
   });
 
   this.get('/projects/:id', function (db, request) {
-    return { data: projects.find((project) => request.params.id === project.id) };
+    return { projects: projects.find((project) => request.params.id === project.id) };
   });
 }
