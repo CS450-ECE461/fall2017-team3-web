34a721e6e2889bb340599c691151ac971f016a50
evrohuff <erh95@att.net>
2017-12-06 03:10:15 -0500

    added comment to image adapter file and added attributes to image model

--- app/adapters/image.js
+++ app/adapters/image.js
@@ -2,4 +2,5 @@ import ApplicationAdapter from './application';
 import FormDataAdapterMixin from 'ember-cli-form-data/mixins/form-data-adapter';
 
 export default ApplicationAdapter.extend (FormDataAdapterMixin, {
+    // FormDataAdapterMixin will give the image model the 'file' attribute
 });
