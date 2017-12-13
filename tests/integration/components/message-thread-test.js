5e027881ea5862859746581a5ddf968fde5154b4
Mounir Karmada <mounkarm@iupui.edu>
2017-12-13 01:39:25 -0500

    cleaned up default test cases

--- tests/integration/components/message-thread-test.js
+++ tests/integration/components/message-thread-test.js
@@ -13,12 +13,4 @@ test('it renders', function(assert) {
 
   assert.equal(this.$().text().trim(), '');
 
-  // Template block usage:
-  this.render(hbs`
-    {{#message-thread}}
-      template block text
-    {{/message-thread}}
-  `);
-
-  assert.equal(this.$().text().trim(), 'template block text');
 });
