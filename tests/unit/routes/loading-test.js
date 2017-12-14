17fa700bca176a04eeca541025f9f3588aa70a4e
Mounir Karmada <mounkarm@iupui.edu>
2017-12-13 19:25:46 -0500

    updated test cases for protected routes

--- tests/unit/routes/loading-test.js
+++ tests/unit/routes/loading-test.js
@@ -1,11 +1,7 @@
-import { moduleFor, test } from 'ember-qunit';
+import { moduleFor } from 'ember-qunit';
 
 moduleFor('route:loading', 'Unit | Route | loading', {
   // Specify the other units that are required for this test.
   // needs: ['controller:foo']
 });
 
-test('it exists', function(assert) {
-  let route = this.subject();
-  assert.ok(route);
-});
